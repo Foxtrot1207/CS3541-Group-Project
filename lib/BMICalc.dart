1f6cb206c6ac5bf0ce449d9e158444826a99a192
sches009 <sches009@d.umn.edu>
2024-03-13 15:20:07 -0500

    added user input

---
+++ lib/BMICalc.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CalcBMI extends StatelessWidget {
+  const CalcBMI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter Height in Inches',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter weight in Pounds',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Current BMI',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
