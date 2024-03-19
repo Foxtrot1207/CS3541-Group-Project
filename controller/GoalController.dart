bbb5ec43dc1718512cea089356a0148337e21831
Emilee <rebho020@d.umn.edu>
2024-03-19 12:02:31 -0500

    Create edit goal function called editGoal

--- controller/GoalController.dart
+++ controller/GoalController.dart
@@ -5,4 +5,10 @@ class GoalController {
     Goal newGoal = Goal(goalName: goalName, goalDescription: goalDescription, deadline: deadline);
   }
 
+  void editGoal(Goal goal, String newGoalName, String newGoalDescription, DateTime newDeadline) {
+    goal.goalName = newGoalName;
+    goal.goalDescription = newGoalDescription;
+    goal.deadline = newDeadline;
+  }
+
 }
