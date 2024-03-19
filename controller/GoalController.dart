58747176ce1ca5d94e947e26bd73f9743fea66d7
Emilee <rebho020@d.umn.edu>
2024-03-19 12:12:43 -0500

    Add database components

--- controller/GoalController.dart
+++ controller/GoalController.dart
@@ -1,8 +1,11 @@
 
 
 class GoalController {
+  final database = Placeholder;
+
   void createGoal(string goalName, String goalDescription, DateTime deadline) {
     Goal newGoal = Goal(goalName: goalName, goalDescription: goalDescription, deadline: deadline);
+    database.insert('goals', newGoal.toMap());
   }
 
   void editGoal(Goal goal, String newGoalName, String newGoalDescription, DateTime newDeadline) {
