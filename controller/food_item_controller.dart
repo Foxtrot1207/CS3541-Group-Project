67ec6e4b80a3e0fd6b00178525f27c69367867bf
soell003 <soell003@d.umn.edu>
2024-03-19 16:46:29 -0500

    Added controller and view for health goals, as well as consolidating files into the food item controller

--- controller/food_item_controller.dart
+++ controller/food_item_controller.dart
@@ -1,15 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'food_item_dao.dart';
-import 'food_item_view.dart';
+import 'food_item.dart';
 
 class FoodItemController with ChangeNotifier {
-  final FoodItemDAO _foodItemDAO = FoodItemDAO();
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
   List<FoodItem> _foodItems = [];
 
   List<FoodItem> get foodItems => _foodItems;
 
+  Future<void> addFoodItem(FoodItem foodItem) async {
+    await _db.collection('foodItems').add(foodItem.toMap());
+    fetchFoodItems(); // Refresh the list after adding a new item
+  }
+
+  Future<List<FoodItem>> getFoodItems() async {
+    QuerySnapshot querySnapshot = await _db.collection('foodItems').get();
+    _foodItems = querySnapshot.docs.map((doc) => FoodItem.fromMap(doc.data())).toList();
+    notifyListeners();
+    return _foodItems;
+  }
+
+
+  Future<void> deleteFoodItem(String id) async {
+    await _db.collection('foodItems').doc(id).delete();
+    fetchFoodItems(); // Refresh the list after deleting an item
+  }
+
   Future<void> fetchFoodItems() async {
-    _foodItems = await _foodItemDAO.getFoodItems();
+    _foodItems = await getFoodItems();
     notifyListeners();
   }
 }
