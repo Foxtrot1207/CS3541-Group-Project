cadaccd4fbac2c6ab121e18962b93a7d4cae6bbc
Emilee <rebho020@d.umn.edu>
2024-03-20 14:47:52 -0500

    Create base UI for Goal Input page

---
+++ lib/GoalInput.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class GoalInput extends StatefulWidget {
+  final String goal;
+
+  GoalInput({this.goal});
+
+  @override
+  _GoalInputState createState() => _GoalInputState();
+}
+
+class _GoalInputState extends State<GoalInput> {
+  TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController(text: widget.goal);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Goal Input'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: 'Goal',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context, controller.text);
+        },
+        child: Icon(Icons.check),
+      ),
+    );
+  }
+}
