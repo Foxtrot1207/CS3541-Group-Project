d2b209b3fb24b0a8be03f380258908642206ac7a
sches009 <sches009@d.umn.edu>
2024-03-20 15:17:41 -0500

    Added Unit Tests and exception cases

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //Initializing screen
   @override
   Widget build(BuildContext context) {
     const appTitle = 'Calculate BMI';
@@ -14,14 +15,45 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(appTitle),
         ),
-        body: const MyCustomForm(),
+        body: const CalcBMIController(),
       ),
     );
   }
 }
 
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
+class CalcBMIModel {
+  double bmiCalc(double weight, double height) {
+    //Handles error cases like 0 or negative numbers.
+    if(weight <= 0 || height <= 0){
+      throw ArgumentError("Weight and height must be greater than zero.");
+    }
+    return ((weight * 703) / (height * height));
+  }
+}
+
+class CalcBMIController extends StatefulWidget {
+  const CalcBMIController({super.key});
+  @override
+  State<CalcBMIController> createState() => _CalcBMIControllerState();
+}
+
+class _CalcBMIControllerState extends State<CalcBMIController> {
+  //controllers take in the user inputted value
+  final heightController = TextEditingController();
+  final weightController = TextEditingController();
+  //String to hold calculated BMI
+  String resultBMI = "";
+
+  void calculateBMI() {
+    //Taking input string into variable
+    final double? height = double.parse(heightController.text);
+    final double? weight = double.parse(weightController.text);
+    final bmi = CalcBMIModel().bmiCalc(weight!, height!);
+    setState(() {
+      //Pop up message with BMI to the the tenth decimal
+      resultBMI = "Your BMI is ${bmi.toStringAsFixed(1)}";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,30 +63,36 @@ class MyCustomForm extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextFormField(
+            controller: heightController, //takes Height value
             decoration: const InputDecoration(
               border: UnderlineInputBorder(),
-              labelText: 'Enter Height in Inches',
+              labelText: 'Enter Height in Inches:',
             ),
+            keyboardType: TextInputType.number, //allows number only keyboard
           ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextFormField(
+            controller: weightController, //takes Weight value
             decoration: const InputDecoration(
               border: UnderlineInputBorder(),
-              labelText: 'Enter weight in Pounds',
+              labelText: 'Enter Weight in Pounds:',
             ),
+            keyboardType: TextInputType.number, //allows numbers only keyboard
           ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Current BMI',
-            ),
-          ),
+          child: Text(resultBMI),
         ),
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          ),
+          onPressed: calculateBMI,
+          child: const Text('Calculate BMI'),
+        )
       ],
     );
   }
---
+++ test/_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:healthapp/main.dart';
+
+void main() {
+  final model = CalcBMIModel();
+
+  test('BMI calculation accuracy', () {
+    double result = model.bmiCalc(160, 60);
+    expect(result, closeTo(31.24, 0.1));
+  });
+
+  test('Exception when weight is zero', () {
+    expect(() => model.bmiCalc(0, 160), throwsA(isA<ArgumentError>()));
+  });
+
+  test('Exception when height is zero', () {
+    expect(() => model.bmiCalc(72, 0), throwsA(isA<ArgumentError>()));
+  });
+
+  test('Exception when weight is negative', () {
+    expect(() => model.bmiCalc(-1, 160), throwsA(isA<ArgumentError>()));
+  });
+
+  test('Exception when height is negative', () {
+    expect(() => model.bmiCalc(72, -1), throwsA(isA<ArgumentError>()));
+  });
+}
