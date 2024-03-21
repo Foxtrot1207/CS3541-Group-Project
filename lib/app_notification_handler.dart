723f7d31b2066e31affcea3e38b69383ea882f82
Ethan Igel <igel0009@umn.edu>
2024-03-21 14:48:43 -0500

    Added the functionality of messagin whether goals are met in the notification titles

--- lib/app_notification_handler.dart
+++ lib/app_notification_handler.dart
@@ -6,12 +6,31 @@ class AppNotificationHandler {
     static final idList = <String, int>{};
 
     void createNotification ( GoalNotification goal ) {
+        String goalTitle;
+        String goalDescription = goal.goalDescription;
+
+        switch ( goal.goalStatus ) {
+            case -1:            
+                goalTitle = "You did not meet your goal of " + goal.goalName;
+                break;
+            case 0:
+                goalTitle = "You achieved your goal of " + goal.goalName;
+                break;
+            case 1:
+                goalTitle = "You surpassed your goal of " + goal.goalName;
+                break;
+            default:
+                goalTitle = "Something went wrong here goalStatus was wrong";
+                goalDescription = goal.goalStatus.toString();
+                break;
+        }
+
         AwesomeNotifications().createNotification(
             content: NotificationContent(
                 id: _idMaker( goal.goalName ),
                 channelKey: _channelKey,
-                title: goal.goalName,
-                body: goal.goalDescription,
+                title: goalTitle,
+                body: goalDescription,
                 category: NotificationCategory.Reminder,
             ),
             schedule: NotificationCalendar.fromDate(date: goal.deadline),
