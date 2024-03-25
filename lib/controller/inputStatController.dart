5e211920facea226e44adf613f779efb2d4d4f23
Taehyun Seok <seok0005@d.umn.edu>
2024-03-25 15:29:26 -0500

    code for input stat

---
+++ lib/controller/inputStatController.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import '../model/inputStatModel.dart';
+import '../view/inputStatView.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print("couldn't connect");
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Input Stat',
+            theme: ThemeData(
+              primarySwatch: Colors.brown,
+            ),
+            home: MyHomePageContainer(),
+          );
+        }
+        return MaterialApp(); // Placeholder for loading state
+      },
+    );
+  }
+}
+
+class MyHomePageContainer extends StatelessWidget {
+  const MyHomePageContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage(
+      title: 'Input Stat about My Food',
+      addStatCallback: (String calories, String nutrition) {
+        StatModel().addStat(calories, nutrition);
+      },
+    );
+  }
+}
