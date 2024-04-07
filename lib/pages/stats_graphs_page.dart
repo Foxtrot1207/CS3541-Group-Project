3e6562e23f6eeff143c05c40db4f8869d6af725d
sches009 <sches009@d.umn.edu>
2024-04-07 12:07:53 -0500

    Finished Stats Drop Down menu

--- lib/pages/stats_graphs_page.dart
+++ lib/pages/stats_graphs_page.dart
@@ -6,39 +6,23 @@ class StatsGraphScreen extends StatefulWidget {
   StatsGraphScreen({required this.controller});
 
   @override
-  _StatsGraphScreenState createState() => _StatsGraphScreenState();
+  _StatsScreenState createState() => _StatsScreenState();
 }
 
-class _StatsGraphScreenState extends State<StatsGraphScreen> {
+class _StatsScreenState extends State<StatsGraphScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
         children: [
-          Placeholder( //Drop down bar and page display
-            //size of box
-            fallbackHeight:50,
-            fallbackWidth:10,
-            //color of box
-            color: Colors.black!,
-          ),
-
-          const SizedBox(height: 100), //Space between placeholders
-
-          Placeholder( //Graph 1 Placeholder
-            fallbackHeight:200, //size of box
-            color: Colors.red!, //color of box
-          ),
-
-          const SizedBox(height: 100), //Space between placeholders
-
-          Placeholder( //Graph 2 Placeholder
-            fallbackHeight:200, //size of box
-            color: Colors.green!, //color of box
-          ),
+        Container(
+        alignment: Alignment.center,
+        child: Text('Graph'),
+          )
         ],
       ),
     );
   }
 }
+
