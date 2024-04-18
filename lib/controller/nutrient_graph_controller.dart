fd132df83576a70111b64be6c7aa60e4f4f89f6e
soell003 <soell003@d.umn.edu>
2024-04-18 13:24:17 -0500

    Added code for fetching firebase data: doesn't yet work in my machine due to an error.

--- lib/controller/nutrient_graph_controller.dart
+++ lib/controller/nutrient_graph_controller.dart
@@ -1,7 +1,38 @@
 import 'package:tuple/tuple.dart';
 import 'package:healthapp/model/nutrient_graph.dart';
 import 'package:healthapp/model/health_goal.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+// import 'firebase_options.dart';
+import 'dart:async';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+
+CollectionReference collectionRef = firestore.collection('Daily Logs-20240417-Food Items');
+
+StreamSubscription<QuerySnapshot> subscription = collectionRef.snapshots().listen((QuerySnapshot snapshot) {
+  // Iterate through the documents in the snapshot
+  for (var doc in snapshot.docs) {
+    // Access the document data using the data() method
+    Object? data = doc.data();
+
+    // Do something with the document data
+    print('Document ID: ${doc.id}');
+    print('Document Data: $data');
+  }
+});
+
+
+
+
+
 
 class NutrientGraphController {
   List<Tuple3<DateTime, double, HealthGoalAttribute>> nutrientData = [];
