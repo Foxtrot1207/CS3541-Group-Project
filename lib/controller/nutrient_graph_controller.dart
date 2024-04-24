a3da22a85bbd0a0f618568c5022345c63aab5eee
soell003 <soell003@d.umn.edu>
2024-04-24 12:35:03 -0500

    graph_fix

--- lib/controller/nutrient_graph_controller.dart
+++ lib/controller/nutrient_graph_controller.dart
@@ -1,3 +1,4 @@
+import 'package:rxdart/rxdart.dart';
 import 'package:tuple/tuple.dart';
 import 'package:healthapp/model/nutrient_graph.dart';
 import 'package:healthapp/model/health_goal.dart';
@@ -8,25 +9,102 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'dart:async';
 
+<<<<<<< Updated upstream
+=======
+import '../view/nutrient_graph_view.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+//get the date now
+// DateTime endDate = DateTime.now();
+// DateTime startDate = endDate.subtract(const Duration(days: 6));
+// // Use DateTime.parse to convert the formatted string to DateTime
+// String formattedStartDateStr = DateFormat('yyyyMMdd').format(startDate);
+// DateTime formattedStartDate = DateFormat('yyyyMMdd').parse(formattedStartDateStr);
+
+DateTime endDate = DateTime.now();
+DateTime startDate = endDate.subtract(const Duration(days: 6));
+String formattedStartDateString = DateFormat('yyyyMMdd').format(startDate);
+DateTime formattedStartDate = DateFormat('yyyyMMdd').parse(formattedStartDateString);
+
+// Initialize an empty list to store the data points
+
+
+>>>>>>> Stashed changes
 class NutrientGraphController {
   List<Tuple3<DateTime, double, HealthGoalAttribute>> nutrientData = [];
   NutrientGraphController({required this.nutrientData});
 
-  void addNutrientData(NutrientData date, value, attribute) {
-    Tuple3<DateTime, double, HealthGoalAttribute> newNutrient = (date, value, attribute) as Tuple3<DateTime, double, HealthGoalAttribute>;
-    nutrientData.add(newNutrient);
+
+  Future<List<Tuple3<String, double, String>>> getLogsForDates() async {
+    List<Tuple3<String, double, String>> dataPoints = [];
+    List<String> formattedDateList = [formattedStartDate.toString(), formattedStartDate.add(const Duration(days: 1)).toString(), formattedStartDate.add(const Duration(days: 2)).toString(), formattedStartDate.add(const Duration(days: 3)).toString(), formattedStartDate.add(const Duration(days: 4)).toString(), formattedStartDate.add(const Duration(days: 5)).toString(), formattedStartDate.add(const Duration(days: 6)).toString()];
+
+    for (String formattedDate in formattedDateList) {
+      Stream<QuerySnapshot> snapshots = FirebaseFirestore.instance
+          .collection('Daily Logs')
+          .doc(formattedDate)
+          .collection('Food Items')
+          .snapshots();
+      await for (var snapshot in snapshots) {
+        for (DocumentSnapshot doc in snapshot.docs) {
+          double itemCarb = doc.get('carbohydrates_g');
+          double itemSugar = doc.get('sugar_g');
+          double itemFat = doc.get('fat_g');
+          double itemProtein = doc.get("protein_g");
+          String itemDate = formattedDate;
+          DateTime formattedStartDate = DateFormat('yyyyMMdd').parse(formattedStartDateString);
+
+          Tuple3<String, double, String> plotItem = (formattedStartDate, itemSugar, 'sugar') as Tuple3<String, double, String>;
+          Tuple3<String, double, String> plotItem2 = (formattedStartDate, itemCarb, 'carbs') as Tuple3<String, double, String>;
+          Tuple3<String, double, String> plotItem3 = (formattedStartDate, itemFat, 'fat') as Tuple3<String, double, String>;
+          Tuple3<String, double, String> plotItem4 = (formattedStartDate, itemProtein, 'protein') as Tuple3<String, double, String>;
+          dataPoints.add(plotItem);
+          dataPoints.add(plotItem2);
+          dataPoints.add(plotItem3);
+          dataPoints.add(plotItem4);
+        }
+      }
+    }
+
+    return dataPoints;
   }
 
-  List<Tuple3<DateTime, double, HealthGoalAttribute>> getNutrientData() {
-    return nutrientData;
-  }
 
-  bool hasDataForSelectedTypes(Map<HealthGoalAttribute, bool> selections) {
-    return nutrientData.any((data) => selections[data.item3] == true);
-  }
+  Future<List<List<ChartData>>> fetchDataAndProcess() async {
 
-  String getAttributeName(int index) {
-    return nutrientData[index].item3.name;
+    Future<List<Tuple3<String, double, String>>> dataPointsFuture = getLogsForDates();
+    List<Tuple3<String, double, String>> dataPoints = await dataPointsFuture;
+
+    List<List<ChartData>> chartDataList = [];
+    chartDataList.add([]); // List for carbs
+    chartDataList.add([]); // List for protein
+    chartDataList.add([]); // List for fat
+    chartDataList.add([]); // List for sugar
+
+    // Now you have the data in dataPoints, process it here
+    for (Tuple3<String, double, String> tuple in dataPoints) {
+      // Access data from each tuple
+      String stringDate = tuple.item1;
+      double value = tuple.item2;
+      String type = tuple.item3;
+      DateTime date = DateTime.parse(stringDate);
+
+      int index = 0;
+      if (type == 'sugar') {
+        index = 2;
+      } else if (type == 'carbs') {
+        index = 0;
+      } else if (type == 'fat') {
+        index = 3;
+      } else if (type == 'protein') {
+        index = 1;
+      }
+      chartDataList[index].add(ChartData(date, value));
+    }
+    return chartDataList; // Return the processed chart data
   }
 }
-
--- lib/view/nutrient_graph_view.dart
+++ lib/view/nutrient_graph_view.dart
@@ -8,6 +8,8 @@ import 'package:intl/intl.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../model/food_item.dart';
+
 class ChartData {
   ChartData(this.x, this.y);
   final DateTime  x;
@@ -22,137 +24,101 @@ class NutrientGraphView extends StatelessWidget {
 
   const NutrientGraphView({super.key, required this.controller});
 
-
   @override
   Widget build(BuildContext context) {
-
-    // DateTime endDate = DateTime.now();
-    // DateTime startDate = endDate.subtract(const Duration(days: 6));
-    //
-    // String formattedStartDate = DateFormat('yyyyMMdd').format(startDate);
-    // String formattedEndDate = DateFormat('yyyyMMdd').format(endDate);
-
-    Future<void> main() async {
-      WidgetsFlutterBinding.ensureInitialized();
-      await Firebase.initializeApp();
-    }
-
-    // final FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-//get the date now
-    DateTime endDate = DateTime.now();
-    DateTime startDate = endDate.subtract(const Duration(days: 6));
-    String formattedStartDate = DateFormat('yyyyMMdd').format(startDate);
-
-    // List<String> dateList = [formattedStartDate.toString(), formattedStartDate.add(const Duration(days: 1)).toString(), formattedStartDate.add(const Duration(days: 2)).toString(), formattedStartDate.add(const Duration(days: 3)).toString(), formattedStartDate.add(const Duration(days: 4)).toString(), formattedStartDate.add(const Duration(days: 5)).toString(), formattedStartDate.add(const Duration(days: 6)).toString()];
-    // final dailyLogsRef = firestore.collection('Daily Logs');
-
-// Initialize an empty list to store the data points
-    List<Tuple2<DateTime, double>> dataPoints = [];
-
-// // Loop through each date in the dateList
-//     for (String date in dateList) {
-// // Get the collection for the current date
-//       final currentDayFood = dailyLogsRef.collection(date);
-// // Get the documents in the 'Food Items' collection
-//       final foodItemsQuery = currentDayFood.collectionGroup('Food Items');
-// // Get the documents
-//       final foodItems = foodItemsQuery.get();
-// // Loop through each document and add its data to the dataPoints list
-//       for (var doc in foodItems.docs) {
-// // Assuming the document has a 'value' field that represents the point you want to plot
-//         double value = doc.data()['value'];
-// // Convert the date string back to a DateTime object for plotting
-//         DateTime dateTime = DateFormat('yyyyMMdd').parse(date);
-// // Add the data point to the list
-//         dataPoints.add(Tuple2(dateTime, value));
-//       }
-//     }
-
-
-// Generate chart data for the last 7 days
-    List<ChartData> chartData = [];
-    List<ChartData> chartData2 = [];
-    List<ChartData> chartData3 = [];
-    List<ChartData> chartData4 = [];
-    for (DateTime date = startDate; date.isBefore(endDate.add(const Duration(days: 1))); date = date.add(const Duration(days: 1))) {
-      // Generate some random values for demonstration
-      double value = Random().nextInt(100).toDouble();  //carbs
-      double value2 = Random().nextInt(100).toDouble();  //protein
-      double value3 = Random().nextInt(55).toDouble(); //sugar
-      double value4 = Random().nextInt(100).toDouble();  //fat
-      chartData.add(ChartData(date, value));
-      chartData2.add(ChartData(date, value2));
-      chartData3.add(ChartData(date, value3));
-      chartData4.add(ChartData(date, value4));
-    }
-
-
     return Scaffold(
-        body: Center(
-            child: SfCartesianChart(
-                primaryXAxis: const DateTimeAxis(
-                  title: AxisTitle(
-                    text: 'Date', // Set the title of the axis
-                    textStyle: TextStyle(
-                      color: Colors.black, // Customize the text color
-                      fontFamily: 'Roboto', // Customize the font family
-                      fontSize: 16, // Customize the font size
-                      fontStyle: FontStyle.normal, // Customize the font style
-                      fontWeight: FontWeight.w400, // Customize the font weight
+      body: Center(
+        child: FutureBuilder<List<List<ChartData>>>(
+          future: controller.fetchDataAndProcess(), // Get the future object
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // Show a loading indicator while waiting
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              // Show an error message if there's an error
+              return Text('Error: ${snapshot.error}');
+            } else {
+              // Access the processed chart data (snapshot.data!)
+              List<List<ChartData>> chartDataList = snapshot.data!;
+
+              // Generate chart data based on chartDataList
+
+              //get the date now
+                //DateTime endDate = DateTime.now();
+                //DateTime startDate = endDate.subtract(const Duration(days: 6));
+                //String formattedStartDate = DateFormat('yyyyMMdd').format(startDate);
+
+              //collect the fooditems
+
+              List<ChartData> chartDataCarbs = chartDataList[0];
+              List<ChartData> chartDataProtein = chartDataList[1];
+              List<ChartData> chartDataSugar = chartDataList[2];
+              List<ChartData> chartDataFat = chartDataList[3];
+
+              return SfCartesianChart(
+                  primaryXAxis: const DateTimeAxis(
+                    title: AxisTitle(
+                      text: 'Date', // Set the title of the axis
+                      textStyle: TextStyle(
+                        color: Colors.black, // Customize the text color
+                        fontFamily: 'Roboto', // Customize the font family
+                        fontSize: 16, // Customize the font size
+                        fontStyle: FontStyle.normal, // Customize the font style
+                        fontWeight: FontWeight.w400, // Customize the font weight
+                      ),
+                    ),
+                    intervalType: DateTimeIntervalType.days, // Set the interval type to days
+                    interval: 1, // Set the interval to 1 day
+                  ),
+                  primaryYAxis: const NumericAxis(
+                    title: AxisTitle(
+                      text: 'Grams', // Set the title of the Y-axis
+                      textStyle: TextStyle(
+                        color: Colors.black, // Customize the text color
+                        fontFamily: 'Roboto', // Customize the font family
+                        fontSize: 16, // Customize the font size
+                        fontStyle: FontStyle.normal, // Customize the font style
+                        fontWeight: FontWeight.w400, // Customize the font weight
+                      ),
                     ),
                   ),
-                  intervalType: DateTimeIntervalType.days, // Set the interval type to days
-                  interval: 1, // Set the interval to 1 day
-                ),
-                primaryYAxis: const NumericAxis(
-                  title: AxisTitle(
-                    text: 'Grams', // Set the title of the Y-axis
-                    textStyle: TextStyle(
-                      color: Colors.black, // Customize the text color
-                      fontFamily: 'Roboto', // Customize the font family
-                      fontSize: 16, // Customize the font size
-                      fontStyle: FontStyle.normal, // Customize the font style
-                      fontWeight: FontWeight.w400, // Customize the font weight
-                    ),
+                  legend: const Legend(
+                      isVisible: true,
+                      title: LegendTitle(
+                          text: "Daily Intake"
+                      )
                   ),
-                ),
-                legend: const Legend(
-                    isVisible: true,
-                    title: LegendTitle(
-                        text: "Daily Intake"
-                    )
-                ),
-                series: <LineSeries>[
-                  LineSeries<ChartData, DateTime>(
-                      dataSource: chartData,
+                  series: <LineSeries>[
+                    LineSeries<ChartData, DateTime>(
+                      dataSource: chartDataCarbs,
                       xValueMapper: (ChartData data, _) => data.x,
                       yValueMapper: (ChartData data, _) => data.y,
                       name: 'Carbs',
-                  ),
-                  LineSeries<ChartData, DateTime>(
-                      dataSource: chartData2,
+                    ),
+                    LineSeries<ChartData, DateTime>(
+                      dataSource: chartDataProtein,
                       xValueMapper: (ChartData data, _) => data.x,
                       yValueMapper: (ChartData data, _) => data.y,
                       name: 'Protein',
-                  ),
-                  LineSeries<ChartData, DateTime>(
-                    dataSource: chartData3,
-                    xValueMapper: (ChartData data, _) => data.x,
-                    yValueMapper: (ChartData data, _) => data.y,
-                    name: 'Sugar',
-                  ),
-                  LineSeries<ChartData, DateTime>(
-                    dataSource: chartData4,
-                    xValueMapper: (ChartData data, _) => data.x,
-                    yValueMapper: (ChartData data, _) => data.y,
-                    name: 'Fat',
-                  ),
-                ]
-            )
-        )
+                    ),
+                    LineSeries<ChartData, DateTime>(
+                      dataSource: chartDataSugar,
+                      xValueMapper: (ChartData data, _) => data.x,
+                      yValueMapper: (ChartData data, _) => data.y,
+                      name: 'Sugar',
+                    ),
+                    LineSeries<ChartData, DateTime>(
+                      dataSource: chartDataFat,
+                      xValueMapper: (ChartData data, _) => data.x,
+                      yValueMapper: (ChartData data, _) => data.y,
+                      name: 'Fat',
+                    ),
+                  ]
+              );
+            }
+          },
+        ),
+      ),
     );
   }
-
-
 }
